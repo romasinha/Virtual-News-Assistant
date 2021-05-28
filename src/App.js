e852c739ad62bd871dcd72baebd9d3fccacd53eb
romasinha <sinha.roma99@gmail.com>
2021-05-28 13:35:10 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards'
 
 const alanKey = '1a6cbc6f03f876ce047818290c32080c2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 
-function App() {
+const App=()=> { 
+
+    const [newsArticles, setNewsArticles] =useState([]);
+
     useEffect(()=>{
         alanBtn({
             key: alanKey,
-            onCommand: (command)=>{
-                if(command==='testCommand'){
-                    alert("This command was executed")
-                }
+            onCommand: ({command, articles})=>{
+                if(command==='newHeadlines'){
+                    setNewsArticles(articles);}
             }
         })
     }, [])
+
     return (
         <div>
             <h1>Alan AI</h1>
+            <NewsCards articles={newsArticles}/>
         </div>
     )
 }
