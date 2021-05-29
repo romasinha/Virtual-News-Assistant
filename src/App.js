4eaf82bbb17e8a78c5d88c1d2a4691bab048b634
romasinha <sinha.roma99@gmail.com>
2021-05-29 22:30:27 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
 import NewsCards from './components/NewsCards/NewsCards'
 import {ReactComponent as Logo} from './images/undraw_newspaper_k72w (1).svg'
 const alanKey = '1a6cbc6f03f876ce047818290c32080c2e956eca572e1d8b807a3e2338fdd0dc/stage';
@@ -13,7 +14,7 @@ const App=()=> {
     useEffect(()=>{
         alanBtn({
             key: alanKey,
-            onCommand: ({command, articles})=>{
+            onCommand: ({command, articles, number})=>{
                 if(command==='newHeadlines'){
                     setNewsArticles(articles);
                     setActiveArticle(-1);
@@ -22,6 +23,19 @@ const App=()=> {
                     setActiveArticle((prevActiveArticle)=>
                         prevActiveArticle+1
                     );
+                }
+                else if(command==='open'){
+                    const parsedNumber = number.length>2? wordsToNumbers(number, {fuzzy: true}): number;
+                    const article = articles[parsedNumber-1];
+
+                    if(parsedNumber>20){
+                        alanBtn().playText('Please try that again');
+                    }
+                    else if(article){
+                        window.open(article.url, '_blank');
+                        alanBtn.playText('Opening');
+                    }
+                    
                 }
             }
         })
